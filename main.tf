5dfd97be4f4e1f484019eac83d82381968d46a94
Jenkins <jenkins@win2k11.>
2024-10-28 03:11:12 +0800

    modified:   README.md

--- main.tf
+++ main.tf
@@ -6,6 +6,6 @@ resource "aws_instance" "TestServer" {
   ami           = "ami-03f584e50b2d32776" # ap-northeast-1
   instance_type = "t2.micro"
   tags = {
-      Name = "TF-KhingVM-01"
+      Name = "TF-KhingVM-02"
   }
 }
