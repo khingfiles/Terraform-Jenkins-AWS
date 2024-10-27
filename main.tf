39c62c6f89f070c5b5b4d34ed2c44dc63068feae
Jenkins <jenkins@win2k11.>
2024-10-28 03:09:56 +0800

    new file:   main.tf

---
+++ main.tf
@@ -0,0 +1,11 @@
+provider "aws" {
+    region = "ap-northeast-1"  
+}
+
+resource "aws_instance" "TestServer" {
+  ami           = "ami-03f584e50b2d32776" # ap-northeast-1
+  instance_type = "t2.micro"
+  tags = {
+      Name = "TF-KhingVM-01"
+  }
+}
